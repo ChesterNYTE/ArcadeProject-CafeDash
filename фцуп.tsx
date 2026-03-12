2e35845f710b31d990d4e28f29fa3bdb6fb83907
ChesterNYTE <platong99457@gmail.com>
2026-03-12 23:56:22 +0300

    Delete фцуп.tsx

--- фцуп.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="roguelikeIndoor_transparent" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="442" columns="26">
- <image source="kenney_roguelike-indoors/Tilesheets/roguelikeIndoor_transparent.png" width="458" height="305"/>
-</tileset>
